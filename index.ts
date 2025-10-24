09cf49721b246adb032a389da06f7f653e487e4b
mtarzi <mtarzibrahem@lfdj.com>
2025-10-24 13:54:28 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+
+
+function subarraysWithSum(nums, target) {
+
+}
+
+
+
+
+const nums = [1, 2, 3, 4, 2];
+const target = 5;
+
+console.log(subarraysWithSum(nums, target));
+
+
+// Exercice 2
+
+async function fetchUrls(urls){
+
+}
+
+
+const urls: string[] = [
+    "https://jsonplaceholder.typicode.com/todos/1",
+    "https://jsonplaceholder.typicode.com/todos/2",
+    "https://jsonplaceholder.typicode.com/todos/3",
+    "https://jsonplaceholder.typicode.com/todos/4",
+    "https://jsonplaceholder.typicode.com/todos/5"
+];
+
